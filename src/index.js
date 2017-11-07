c840f1ecc567526fccda5787c460d515a0b79029
unknown <Administrator@HP6SA595T24P68B.zhaopin.com.cn>
2017-11-07 13:36:23 +0800

    client

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Test from './test1'
+import Clock from './ClockClass'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+function tick() {
+    // const element=(
+    //     <div>
+    //         <h1>Hello, world!  {a} {1+1} </h1>
+    //         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    //     </div>
+    // )
+    let children = (
+        <div>
+            <Test />
+            <Clock cishu={1} />
+            <Clock cishu={2}/>
+            <Clock cishu={3} />
+            <Clock  cishu={4}/>
+        </div>
+    );
+    const element = React.createElement("div", { className: 'greeting', datak: '1' }, children);
+
+    ReactDOM.render(element, document.getElementById('root'));
+
+}
+setInterval(tick, 1000);
+
+
+
+function Life(name) {
+    this.name = name;
+    this.className = "earth";
+}
+Life.prototype.say = function () { console.log("I'm "+this.className+";My name is "+ this.name); };
+function Person(name, nation) {
+    Life.apply(this, [name, "person"]);
+    this.Nation = nation;
+};
+
+
+function inherit(subType, superType) {
+    function object(o) {
+        function F() { };
+        F.prototype = o;
+        return new F();
+    }
+    var prototype = object(superType.prototype);
+    subType.prototype = prototype;
+    subType.prototype.constructor = subType;
+}
+inherit(Person,Life);
+Person.prototype.say=function(){ console.log("I'm "+this.className+" and I'm  "+this.Nation+" My name is "+ this.name);}
+function test(p) {
+    if (p instanceof Life) {
+        p.say();
+    }
+}
+test(new Person("rex","chinese"));
+
---
+++ src/test1.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Test extends Component {
+    constructor(){
+        super();
+        this.state={a:1};
+    }
+    render() {
+        return <h1>Hello, world  
+            1+1
+            </h1>
+    }
+}
+    export default Test;
