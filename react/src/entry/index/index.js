f456ce6a9f6fefaa4845d69610e54d26e1078256
rex yang <rexyangluck@live.com>
2018-01-08 15:55:32 +0800

    提交

--- react/src/entry/index/index.js
+++ react/src/entry/index/index.js
@@ -16,7 +16,7 @@ function isWeiXin() {
         return false;
     }
 }
-if (isWeiXin()) {
+// if (isWeiXin()) {
 let url = window.location.href.split('#')[0];
 
 axios.post(config.url + "/token/getshareconfig", { url: url }).then((response) => {
@@ -24,7 +24,7 @@ axios.post(config.url + "/token/getshareconfig", { url: url }).then((response) =
         let data = response.data.data;
 
         wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: data.appId, // 必填，公众号的唯一标识
             timestamp: data.timestamp, // 必填，生成签名的时间戳
             nonceStr: data.nonceStr, // 必填，生成签名的随机串
@@ -67,7 +67,7 @@ wx.ready(function () {
     wx.onMenuShareWeibo(shareConfig);
     wx.onMenuShareQZone(shareConfig);
 });
-}
+// }
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
