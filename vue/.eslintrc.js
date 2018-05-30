194423c67c326768c0abfb998d2ab9f21a6b31bf
rex yang <rexyangluck@live.com>
2018-05-30 16:56:12 +0800

    提交

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
+  
   // required to lint *.vue files
   plugins: [
     'html'
--- vue/src/main.js
+++ vue/src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+const Plugin1 = {
+  install: function () {
+    console.log(arguments)
+  }
+}
+Vue.use(Plugin1)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
