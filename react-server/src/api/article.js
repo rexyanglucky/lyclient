0cd318f3f9487694b87de0bd6ce32638a6a1c2e6
rex <rex@localhost.localdomain>
2018-06-04 18:39:53 +0800

    submit

--- react-server/src/api/article.js
+++ react-server/src/api/article.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '@/config';
 function getArticleList(callback){
-    axios.get('https:'+config.url + "/article/list").then((response) => {
+    axios.get(config.url + "/article/list").then((response) => {
         if (response.data) {
             let data = response.data.data;
             // self.setState({ articleList: data });
@@ -13,7 +13,7 @@ function getArticleList(callback){
     });
 }
 function getArticleDetial(id,callback){
-    axios.get('https:'+config.url + "/article/detial", { params: { id: id } }).then((response) => {
+    axios.get(config.url + "/article/detial", { params: { id: id } }).then((response) => {
         if (response.data) {
             let data = response.data.data;
             if (data.length > 0) {
